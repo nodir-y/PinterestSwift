d79f6232b7ffb19d27950d1aed7b1ae78cba0d27
NY <nodir9999999@gmail.com>
2015-09-13 17:24:30 +0500

    swift 2.0

--- PinterestSwift/CHTCollectionViewWaterfallLayout.swift
+++ PinterestSwift/CHTCollectionViewWaterfallLayout.swift
@@ -108,17 +108,17 @@ class CHTCollectionViewWaterfallLayout : UICollectionViewLayout{
         super.init()
     }
     
-    required init(coder aDecoder: NSCoder) {
+    required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     func itemWidthInSectionAtIndex (section : NSInteger) -> CGFloat {
-        var insets : UIEdgeInsets
-        if let sectionInsets = self.delegate?.colletionView?(self.collectionView!, layout: self, insetForSectionAtIndex: section){
-            insets = sectionInsets
-        }else{
-            insets = self.sectionInset
-        }
+//        var insets : UIEdgeInsets
+//        if let sectionInsets = self.delegate?.colletionView?(self.collectionView!, layout: self, insetForSectionAtIndex: section){
+//            insets = sectionInsets
+//        }else{
+//            insets = self.sectionInset
+//        }
         let width:CGFloat = self.collectionView!.frame.size.width - sectionInset.left-sectionInset.right
         let spaceColumCount:CGFloat = CGFloat(self.columnCount-1)
         return floor((width - (spaceColumCount*self.minimumColumnSpacing)) / CGFloat(self.columnCount))
@@ -159,12 +159,12 @@ class CHTCollectionViewWaterfallLayout : UICollectionViewLayout{
                 minimumInteritemSpacing = self.minimumColumnSpacing
             }
             
-            var sectionInsets :  UIEdgeInsets
-            if let insets = self.delegate?.colletionView?(self.collectionView!, layout: self, insetForSectionAtIndex: section){
-                sectionInsets = insets
-            }else{
-                sectionInsets = self.sectionInset
-            }
+//            var sectionInsets :  UIEdgeInsets
+//            if let insets = self.delegate?.colletionView?(self.collectionView!, layout: self, insetForSectionAtIndex: section){
+//                sectionInsets = insets
+//            }else{
+//                sectionInsets = self.sectionInset
+//            }
             
             let width = self.collectionView!.frame.size.width - sectionInset.left - sectionInset.right
             let spaceColumCount = CGFloat(self.columnCount-1)
@@ -249,16 +249,16 @@ class CHTCollectionViewWaterfallLayout : UICollectionViewLayout{
         idx = 0;
         let itemCounts = self.allItemAttributes.count
         while(idx < itemCounts){
-            var rect1 = self.allItemAttributes.objectAtIndex(idx).frame as CGRect
+            let rect1 = self.allItemAttributes.objectAtIndex(idx).frame as CGRect
             idx = min(idx + unionSize, itemCounts) - 1
-            var rect2 = self.allItemAttributes.objectAtIndex(idx).frame as CGRect
+            let rect2 = self.allItemAttributes.objectAtIndex(idx).frame as CGRect
             self.unionRects.addObject(NSValue(CGRect:CGRectUnion(rect1,rect2)))
             idx++
         }
     }
     
     override func collectionViewContentSize() -> CGSize{
-        var numberOfSections = self.collectionView!.numberOfSections()
+        let numberOfSections = self.collectionView!.numberOfSections()
         if numberOfSections == 0{
             return CGSizeZero
         }
@@ -269,14 +269,14 @@ class CHTCollectionViewWaterfallLayout : UICollectionViewLayout{
         return  contentSize
     }
     
-    override func layoutAttributesForItemAtIndexPath(indexPath: NSIndexPath) -> UICollectionViewLayoutAttributes!{
+    override func layoutAttributesForItemAtIndexPath(indexPath: NSIndexPath) -> (UICollectionViewLayoutAttributes!){
         if indexPath.section >= self.sectionItemAttributes.count{
             return nil
         }
         if indexPath.item >= self.sectionItemAttributes.objectAtIndex(indexPath.section).count{
             return nil;
         }
-        var list = self.sectionItemAttributes.objectAtIndex(indexPath.section) as! NSArray
+        let list = self.sectionItemAttributes.objectAtIndex(indexPath.section) as! NSArray
         return list.objectAtIndex(indexPath.item) as! UICollectionViewLayoutAttributes
     }
     
@@ -290,35 +290,35 @@ class CHTCollectionViewWaterfallLayout : UICollectionViewLayout{
         return attribute
     }
     
-    override func layoutAttributesForElementsInRect (rect : CGRect) -> [AnyObject] {
-        var i = 0
+    override func layoutAttributesForElementsInRect (rect : CGRect) -> [UICollectionViewLayoutAttributes] {
+        //var i = 0
         var begin = 0, end = self.unionRects.count
-        var attrs = NSMutableArray()
+        var attrs = [UICollectionViewLayoutAttributes]()
         
         for var i = 0; i < end; i++ {
-            if CGRectIntersectsRect(rect, self.unionRects.objectAtIndex(i).CGRectValue()){
+            if CGRectIntersectsRect(rect, self.unionRects.objectAtIndex(i).CGRectValue){
                 begin = i * unionSize;
                 break
             }
         }
         for var i = self.unionRects.count - 1; i>=0; i-- {
-            if CGRectIntersectsRect(rect, self.unionRects.objectAtIndex(i).CGRectValue()){
+            if CGRectIntersectsRect(rect, self.unionRects.objectAtIndex(i).CGRectValue){
                 end = min((i+1)*unionSize,self.allItemAttributes.count)
                 break
             }
         }
         for var i = begin; i < end; i++ {
-            var attr = self.allItemAttributes.objectAtIndex(i) as! UICollectionViewLayoutAttributes
+            let attr = self.allItemAttributes.objectAtIndex(i) as! UICollectionViewLayoutAttributes
             if CGRectIntersectsRect(rect, attr.frame) {
-                attrs.addObject(attr)
+                attrs.append(attr)
             }
         }
             
-        return NSArray(array: attrs) as [AnyObject]
+        return attrs
     }
     
     override func shouldInvalidateLayoutForBoundsChange (newBounds : CGRect) -> Bool {
-        var oldBounds = self.collectionView!.bounds
+        let oldBounds = self.collectionView!.bounds
         if CGRectGetWidth(newBounds) != CGRectGetWidth(oldBounds){
             return true
         }
@@ -335,7 +335,7 @@ class CHTCollectionViewWaterfallLayout : UICollectionViewLayout{
         var index = 0
         var shorestHeight = MAXFLOAT
 
-        self.columnHeights.enumerateObjectsUsingBlock({(object : AnyObject!, idx : NSInteger,pointer :UnsafeMutablePointer<ObjCBool>) in
+        self.columnHeights.enumerateObjectsUsingBlock({(object : AnyObject, idx : NSInteger,pointer :UnsafeMutablePointer<ObjCBool>) in
             let height = object.floatValue
             if (height<shorestHeight){
                 shorestHeight = height
@@ -355,7 +355,7 @@ class CHTCollectionViewWaterfallLayout : UICollectionViewLayout{
         var index = 0
         var longestHeight:CGFloat = 0.0
         
-        self.columnHeights.enumerateObjectsUsingBlock({(object : AnyObject!, idx : NSInteger,pointer :UnsafeMutablePointer<ObjCBool>) in
+        self.columnHeights.enumerateObjectsUsingBlock({(object : AnyObject, idx : NSInteger,pointer :UnsafeMutablePointer<ObjCBool>) in
             let height = CGFloat(object.floatValue)
             if (height > longestHeight){
                 longestHeight = height
@@ -379,8 +379,8 @@ class CHTCollectionViewWaterfallLayout : UICollectionViewLayout{
             index = (item%self.columnCount)
         case .CHTCollectionViewWaterfallLayoutItemRenderDirectionRightToLeft:
             index = (self.columnCount - 1) - (item % self.columnCount);
-        default:
-            index = self.shortestColumnIndex()
+//        default:
+//            index = self.shortestColumnIndex()
         }
         return index
     }
--- PinterestSwift/NTTransition.swift
+++ PinterestSwift/NTTransition.swift
@@ -15,7 +15,7 @@ let animationScale = screenWidth/gridWidth // screenWidth / the width of waterfa
 class NTTransition : NSObject , UIViewControllerAnimatedTransitioning{
     var presenting = false
     
-    func transitionDuration(transitionContext: UIViewControllerContextTransitioning) -> NSTimeInterval{
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval{
         return animationDuration
     }
     
@@ -26,7 +26,7 @@ class NTTransition : NSObject , UIViewControllerAnimatedTransitioning{
 
         if presenting {
             let toView = toViewController.view
-            containerView.addSubview(toView)
+            containerView?.addSubview(toView)
             toView.hidden = true
             
             let waterFallView = (toViewController as! NTTransitionProtocol).transitionCollectionView()
@@ -41,7 +41,7 @@ class NTTransition : NSObject , UIViewControllerAnimatedTransitioning{
             let pullOffsetY = (fromViewController as! NTHorizontalPageViewControllerProtocol).pageViewCellScrollViewContentOffset().y
             let offsetY : CGFloat = fromViewController.navigationController!.navigationBarHidden ? 0.0 : navigationHeaderAndStatusbarHeight
             snapShot.origin(CGPointMake(0, -pullOffsetY+offsetY))
-            containerView.addSubview(snapShot)
+            containerView?.addSubview(snapShot)
             
             toView.hidden = false
             toView.alpha = 0
@@ -50,8 +50,8 @@ class NTTransition : NSObject , UIViewControllerAnimatedTransitioning{
                 toView.frame.size.width, toView.frame.size.height)
             let whiteViewContainer = UIView(frame: screenBounds)
             whiteViewContainer.backgroundColor = UIColor.whiteColor()
-            containerView.addSubview(snapShot)
-            containerView.insertSubview(whiteViewContainer, belowSubview: toView)
+            containerView?.addSubview(snapShot)
+            containerView?.insertSubview(whiteViewContainer, belowSubview: toView)
             
             UIView.animateWithDuration(animationDuration, animations: {
                 snapShot.transform = CGAffineTransformIdentity
@@ -73,8 +73,8 @@ class NTTransition : NSObject , UIViewControllerAnimatedTransitioning{
             let waterFallView : UICollectionView = (fromViewController as! NTTransitionProtocol).transitionCollectionView()
             let pageView : UICollectionView = (toViewController as! NTTransitionProtocol).transitionCollectionView()
             
-            containerView.addSubview(fromView)
-            containerView.addSubview(toView)
+            containerView?.addSubview(fromView)
+            containerView?.addSubview(toView)
             
             let indexPath = waterFallView.toIndexPath()
             let gridView = waterFallView.cellForItemAtIndexPath(indexPath)
@@ -87,7 +87,7 @@ class NTTransition : NSObject , UIViewControllerAnimatedTransitioning{
             let offsetStatuBar : CGFloat = fromViewController.navigationController!.navigationBarHidden ? 0.0 :
                 statubarHeight;
             let snapShot = (gridView as! NTTansitionWaterfallGridViewProtocol).snapShotForTransition()
-            containerView.addSubview(snapShot)
+            containerView?.addSubview(snapShot)
             snapShot.origin(leftUpperPoint)
             UIView.animateWithDuration(animationDuration, animations: {
                 snapShot.transform = CGAffineTransformMakeScale(animationScale,
